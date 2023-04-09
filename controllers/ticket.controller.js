16cd6c34bd1bc308917fef7903401094dc43b1fe
Saurabh Purohit <saurabhprht@gamil.com>
2023-04-09 10:45:30 +0530

    Find ticket API created

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -1,6 +1,8 @@
 const Ticket = require(`../models/ticket.model`);
 const User = require(`../models/user.model`)
 const objectConverter = require(`../utils/objectConverter`);
+const CONSTANTS = require(`../utils/constants`);
+const constants = require("../utils/constants");
 
 exports.createTicket = async (req, res) => {
     const ticketObj = {
@@ -35,4 +37,55 @@ exports.createTicket = async (req, res) => {
             message: `Internal server error.`
         })
     }
+}
+
+exports.updateTicket = async (req, res) => {
+    const ticketId = req.params.id;
+    const ticket = await Ticket.findOne({ _id: ticketId });
+
+    if (!ticket) {
+        res.status(400).send({
+            message: `Ticket with the ID ${ticketId} is not present.`
+        })
+    }
+
+    const savedUSer = await User.findOne({ userId: req.userId });
+
+    if (ticket.reporter == req.userId || ticket.assignee == req.userId || savedUSer.userType == CONSTANTS.userType.admin) {
+        ticket.title = req.body.title != undefined ? req.body.title : ticket.title;
+        ticket.description = req.body.description != undefined ? req.body.description : ticket.description;
+        ticket.ticketPriority = req.body.ticketPriority != undefined ? req.body.ticketPriority : ticket.ticketPriority;
+        ticket.status = req.body.status != undefined ? req.body.status : ticket.status;
+        ticket.assignee = req.body.assignee != undefined ? req.body.assignee : ticket.assignee;
+
+        var updatedTicket = await ticket.save();
+
+        res.status(200).send(objectConverter.ticketResponse(updatedTicket));
+    } else {
+        res.status(401).send({
+            message: `Ticket can only be updated by a CUSTOMER or ENGINEER and an ADMIN`
+        })
+    }
+}
+
+exports.getAllTickets = async (req, res) => {
+    const queryObj = {}
+
+    if(req.query.status != undefined) {
+        queryObj.status = {$regex: new RegExp(req.query.status), $options: `i`};
+    }
+
+    const savedUser = await User.findOne({ userId : req.userId })
+
+    if(savedUser.userType == constants.userType.engineer) {
+        queryObj.assignee = req.userId
+    } else if(savedUser.userType == constants.userType.customer){
+        queryObj.reporter = req.userId
+    } else {
+
+    }
+
+    const tickets = await Ticket.find(queryObj);
+
+    res.status(200).send(tickets);
 }
--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -4,4 +4,8 @@ const verifyRequestMiddleware = require('../middlewares/verifyTicketRequestBody'
 
 module.exports = function (app) {
     app.post("/crm/api/v1/ticket",[authMiddleware.verifyToken, verifyRequestMiddleware.validateTicketReq], ticketController.createTicket);
+
+    app.put("/crm/api/v1/ticket/:id",[authMiddleware.verifyToken, verifyRequestMiddleware.validateTicketReq], ticketController.updateTicket);
+
+    app.get("/crm/api/v1/tickets",authMiddleware.verifyToken, ticketController.getAllTickets);
 } 
